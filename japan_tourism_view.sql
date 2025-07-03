06951bc6bb4c5c681667a6706e69e154ead52fd1
Isaac Jeon <43486715+isaacjeon@users.noreply.github.com>
2025-07-02 17:40:38 -0700

    Add files via upload
    Add SQL file containing MySQL script for creating view using tables from csv files from data folder.

---
+++ japan_tourism_view.sql
@@ -0,0 +1,101 @@
+/*
+	Create a view of Japan tourism-related tables that categorizes data
+	by location, geographic_level (Prefecture or Region), category (e.g.
+	Nationality), label (e.g. South Korea), and percentage.
+*/
+CREATE OR REPLACE VIEW japan_tourism_stats AS
+-- Combine tables using generalized 'category' and 'label columns'
+WITH all_categories AS (
+	SELECT `Area of Visit`,
+		'Accommodation Type' AS category,
+		`Accommodation Type` AS label,
+		Percentage AS percentage
+	FROM `accommodation type`
+    UNION ALL
+    	SELECT `Area of Visit`,
+		'Length of Stay' AS category,
+		`Length of Stay` AS label,
+		Percentage AS percentage
+	FROM `length of stay`
+    UNION ALL
+	SELECT `Area of Visit`,
+		'Means of Transportation' AS category,
+		`Means of Transportation` AS label,
+		Percentage AS percentage
+	FROM `means of transportation`
+    UNION ALL
+	SELECT `Area of Visit`,
+		'Nationality' AS category,
+		`Nationality` AS label,
+		Percentage AS percentage
+	FROM `nationality`
+    UNION ALL
+	SELECT `Area of Visit`,
+		'Places to Shop' AS category,
+		`Places to Shop` AS label,
+		Percentage AS percentage
+	FROM `places to shop`
+    UNION ALL
+	SELECT `Area of Visit`,
+		'Port of Departure' AS category,
+		`Port of Departure` AS label,
+		Percentage AS percentage
+	FROM `port of departure`
+    UNION ALL
+	SELECT `Area of Visit`,
+		'Port of Entry' AS category,
+		`port of Entry` AS label,
+		Percentage AS percentage
+	FROM `port of entry`
+    UNION ALL
+	SELECT `Area of Visit`,
+		'Sex/Age' AS category,
+		`Sex/Age` AS label,
+		Percentage AS percentage
+	FROM `sex and age`
+    UNION ALL
+    -- Split 'Sex' and 'Age' into separate categories
+    SELECT `Area of Visit`,
+		'Sex' AS category,
+		SUBSTRING_INDEX(`Sex/Age`, '/', 1) AS label,
+		ROUND(SUM(Percentage), 4) AS percentage
+	FROM `sex and age`
+	GROUP BY `Area of Visit`, label
+	UNION ALL
+    SELECT `Area of Visit`,
+		'Age' AS category,
+		SUBSTRING_INDEX(`Sex/Age`, '/', -1) AS label,
+		ROUND(SUM(Percentage), 4) AS percentage
+	FROM `sex and age`
+	GROUP BY `Area of Visit`, label
+	UNION ALL
+	SELECT `Area of Visit`,
+		'Time Visited Japan' AS category,
+		`Times Visited Japan` AS label,
+		Percentage AS percentage
+	FROM `times visited japan`
+    UNION ALL
+	SELECT `Area of Visit`,
+		'Travel Arrangement' AS category,
+		`Travel Arrangement` AS label,
+		Percentage AS percentage
+	FROM `travel arrangement`
+    UNION ALL
+	SELECT `Area of Visit`,
+		'Travel Companions' AS category,
+		`Travel Companions` AS label,
+		Percentage AS percentage
+	FROM `travel companions`
+)
+-- Distinguish `Area of Visit` between prefectural and regional level
+SELECT
+	(CASE
+		WHEN `Area of Visit` NOT LIKE '%Region' THEN `Area of Visit`
+		ELSE LEFT(`Area of Visit`, LENGTH(`Area of Visit`) - 7)
+    END) AS location,
+	(CASE
+		WHEN `Area of Visit` NOT LIKE '%Region' THEN 'Prefecture'
+		ELSE 'Region'
+    END) AS geographic_level,  
+    category, label, percentage
+FROM all_categories;
